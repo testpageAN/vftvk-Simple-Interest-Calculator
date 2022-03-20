9b6cf3ca69a11a4011e7fe95a71dcb85773d0428
testpageAN <alexis.nikolopoulos@hotmail.com>
2022-03-20 13:58:00 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function compute()
-{
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
+  }
+  
+  function compute() {
     p = document.getElementById("principal").value;
-    
-}
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+    interest = (p * years * rate) / 100;
+    year = new Date().getFullYear() + parseInt(years);
+    if (p <= 0) {
+      alert("Enter a positive number");
+    } else {
+      document.getElementById(
+        "result"
+      ).innerHTML = `<br/> If you deposit ${p}, <br/> 
+      at an interest rate of ${rate}%. <br/> 
+        You will receive an amount of ${interest}, <br/> 
+        in the year ${year}.<br/>`;
+    }
+  }
         
